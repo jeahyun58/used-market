b0f033432c19bef900d6ccb18f918f5a1016f0c8
hyun530 <ljhyun530@gmail.com>
2022-07-17 16:46:10 +0900

    #8_3_CleanCode

---
+++ note/#8_3_CleanCode.md
@@ -0,0 +1,29 @@
+# 8_3_CleanCode
+
+## section.log
+
+- Clean code
+- Create a hook that we can use.
+- API로 DATA를 POST 하기위한 hook.
+
+## tips
+
+- none
+
+## issue
+
+- none
+
+## links
+
+- none
+
+## added dependencies
+
+- none
+
+### dependencies
+
+- none
+
+### devDependencies
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../components/button";
 import Input from "../components/input";
-import { cls } from "../libs/utils";
+import useMutation from "../libs/client/useMutaion";
+import { cls } from "../libs/client/utils";
 
 interface EnterForm {
   email?: string;
@@ -11,6 +12,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
+  const [enter, { loading, data, error }] = useMutation("/api/users/enter"); //어떤 url을 mutate할지 알아야함
   const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
@@ -22,18 +24,10 @@ const Enter: NextPage = () => {
     reset();
     setMethod("phone");
   };
-  const onValid = (data: EnterForm) => {
-    setSubmitting(true);
-    fetch("/api/users/enter", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then(() => {
-      setSubmitting(false);
-    });
+  const onValid = (validForm: EnterForm) => {
+    enter(validForm);
   };
+  console.log(loading, data, error);
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
