6d37d83a94759c4c4c91a2e72176292010183f82
hyun530 <ljhyun530@gmail.com>
2022-07-30 22:41:40 +0900

    #10_4_useUser_SWR

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,18 +1,15 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
   useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter"); //push -> replace 브라우저에게 홈 화면 히스토리를 기록하지 말것
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+    if (data && !data.ok) {
+      router.replace("/enter"); //push -> replace 브라우저에게 홈 화면 히스토리를 기록하지 말것
+    }
+  }, [data, router]);
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
---
+++ note/#10_4_useUser_SWR.md
@@ -0,0 +1,30 @@
+# 10_4_useUser_SWR
+
+## section.log
+
+- `useUser.ts`를 SWR을 이용하여 쉽게 사용하도록 구현
+
+- React Query가 더 많은 기능을 제공하고 더 좋지만 그만큼 더많은 용량을 차지함.
+  > 따라서 간단한 용도의 App에는 SWR사용이 좋지만 그이외에는 React Query 사용을 추천함
+
+## tips
+
+- none
+
+## issue
+
+- none
+
+## links
+
+- none
+
+## added dependencies
+
+- none
+
+### dependencies
+
+- none
+
+### devDependencies
